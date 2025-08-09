dd852cfdb21f622fd99d0b371654d68f88b74cda
Martin Simonsen <marwynskyggfisk@gmail.com>
2025-08-09 18:36:51 +0200

    add guess animation

--- scripts/guess-row.js
+++ scripts/guess-row.js
@@ -9,18 +9,11 @@ class GuessRow extends HTMLElement {
     createTemplate() {
         return `
             <fieldset class="inputContainer">
-                <div class="inputWrapper">
-                    <input class="guess" type="text" inputmode="numeric" maxlength="1" value="0" />
-                </div>
-                <div class="inputWrapper">
-                    <input class="guess" type="text" inputmode="numeric" maxlength="1" value="0" />
-                </div>
-                <div class="inputWrapper">
-                    <input class="guess" type="text" inputmode="numeric" maxlength="1" value="0" />
-                </div>
-                <div class="inputWrapper">
-                    <input class="guess" type="text" inputmode="numeric" maxlength="1" value="0" />
-                </div>
+                ${[0, 1, 2, 3].map(i => `
+                    <div class="inputWrapper">
+                        <input class="guess" type="text" inputmode="numeric" maxlength="1" value="0" style="--index: ${i}" />
+                    </div>
+                    `).join("")}
                 <button type="submit" style="position: absolute; left: -9999px; width: 1px; height: 1px;"></button>
             </fieldset>
         `
